a7748b7bcfd769a6deb66e03860845067963db49
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-07-10 00:02:00 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+var names = ["Dua Fatima", "Sakina", "Sarfaraz"];
+var message = "When we make next plan to go out for lunch after the class?";
+console.log(names + " " + message);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names : string [] = ["Dua Fatima","Sakina","Sarfaraz"];
+
+let message : string = "When we make next plan to go out for lunch after the class?"
+console.log(names + " "+ message);
